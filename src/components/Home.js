089e5a2cf438b0dd6b647a46fd1583b4a311c62a
MaksZys <maks.zys.elk@gmail.com>
2018-10-19 20:58:36 +0200

    Add styled components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import { Button, Icon } from '@blueprintjs/core';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>{this.props.msg}</h1>
+        <Button>Text in button</Button>
+        <Icon icon="clipboard"/>
       </div>
     );
   }
